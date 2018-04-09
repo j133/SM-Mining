17f0dd9e4de25371d9839ecef78fcd40100d78ae
Janice Lee <janiclee@gmail.com>
2018-04-09 16:58:31 -0400

    SM Homework

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "oenujvwvbd3j2uf9PGd06VciQ"
+consumer_secret = "YmLrx8TVze72yEskqi4nDmWGLNTXCY6yWOuFotrqammWCvM8jc"
+access_token = "979124797891375104-QTjzVJzeP5qCChrXzj8jCIFvi3pnpWC"
+access_token_secret = "CMyQwsZm302pqXSO0EQXLgLpG7Mle6HOkd00k4SSt5M2i"
